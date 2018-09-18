a34322da44507100ec67701734190be463b537d3
maceklu161 <maceklu161@DST20703>
2018-09-18 10:24:44 +0200

    d

--- GitHub1/GitHub1/Lod.cs
+++ GitHub1/GitHub1/Lod.cs
@@ -8,6 +8,42 @@ namespace GitHub1
 {
     class Lod
     {
-        
+        public string oznaceni;
+        public string stav;
+        private int velikost;
+        List<Lod> lode = new List<Lod>();
+        public Lod()
+        {
+            lode.Add(new Lod
+            {
+                oznaceni = "ponorka",
+                velikost = 1,
+                stav = String.Empty
+            });
+            lode.Add(new Lod
+            {
+                oznaceni = "torpédoborec",
+                velikost = 2,
+                stav = String.Empty
+            });
+            lode.Add(new Lod
+            {
+                oznaceni = "křižník",
+                velikost = 3,
+                stav = String.Empty
+            });
+            lode.Add(new Lod
+            {
+                oznaceni = "bitevní loď",
+                velikost = 4,
+                stav = String.Empty
+            });
+            lode.Add(new Lod
+            {
+                oznaceni = "letadlová loď",
+                velikost = 5,
+                stav = String.Empty
+            });
+        }
     }
 }
